4137f1a3bfa13c765c880645a81643931731e44b
entymon <pawel.olejniczak.i@gmail.com>
2018-09-10 14:25:42 +0100

    1. Change wordpress app port to :8001 2. Added PHPMyAdmin on port :8181

--- sql/03_update_db.sql
+++ sql/03_update_db.sql
@@ -1,4 +1,4 @@
 USE wordpress;
-update wp_options set option_value="http://localhost" where option_name = "siteurl";
-update wp_options set option_value="http://localhost" where option_name = "home";
+update wp_options set option_value="http://127.0.0.1:8001" where option_name = "siteurl";
+update wp_options set option_value="http://127.0.0.1:8001" where option_name = "home";
 update wp_options set option_value="no" where option_name = "woocommerce_force_ssl_checkout";
